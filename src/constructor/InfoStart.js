3fb6f23b42a4f1a6eb611bc0e63cf6e505db4882
Sergey Maslinikov <maslinikovsergey@gmail.com>
2021-09-29 11:40:13 -0400

    yeah

---
+++ src/constructor/InfoStart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function InfoStart() {
+    return(
+        <div className='start_img'>
+            <img src='./istockphoto-499906780-1024x1024.jpg' alt='Logo' />
+        </div>
+    )
+}
+
+export default InfoStart;
---
+++ src/constructor/StartPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function StartPage() {
+    return ( 
+            <div className='main_cont'>
+                <h1>Make it happening now</h1>
+                <h3>Join our coffee break community</h3>
+                <button className='btn_sart sign_in'>Sign in</button>
+                <button className='btn_sart sign_up'>Sign Up</button>
+            </div>
+    );
+}
+
+export default StartPage;
